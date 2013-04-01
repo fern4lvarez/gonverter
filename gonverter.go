0833d3a6f1f883454efd9809dfa158536f886879
Fernando <alvarez.calleja.fernando@gmail.com>
2013-04-01 11:56:36 +0200

    New method names, new comments, new gofmt

--- gonverter.go
+++ gonverter.go
@@ -1,17 +1,19 @@
 package gonverter
 
 import (
-  "strconv"
+	"strconv"
 )
 
-func Sti(s string) int {
-  var i int
-  i, _ = strconv.Atoi(s)
-  return i
+// Converts a `string` into a `int`
+func StoI(s string) int {
+	var i int
+	i, _ = strconv.Atoi(s)
+	return i
 }
 
-func Its(i int) string {
-  var s string
-  s = strconv.Itoa(i)
-  return s
+// Converts a `int` into a `string`
+func ItoS(i int) string {
+	var s string
+	s = strconv.Itoa(i)
+	return s
 }
