be7a0721d710205be2bce0e8bfcbe4e2dd727bf6
fern4lvarez <alvarez.calleja.fernando@gmail.com>
2013-04-02 17:13:45 +0200

    New bool related conversions, fixups

--- gonverter.go
+++ gonverter.go
@@ -21,3 +21,34 @@ func ItoS(i int) string {
 	s := strconv.Itoa(i)
 	return s
 }
+
+// Converts a `int` into a `bool`
+// If i is 0, return false, otherwise returns true
+func ItoB(i int) bool {
+	return i != 0
+}
+
+// Converts a `bool` into a `int`
+// If b is true returns 1, if false returns 0
+func BtoI(b bool) int {
+	if b {
+		return 1
+	}
+	return 0
+}
+
+// Converts a `string` into a `bool`
+// If s is empty, return false, otherwise returns true
+func StoB(s string) bool {
+	return s != ""
+}
+
+// Converts a `bool` into a `string`
+// If b is true return "true", if false returns "false"
+// Note: It is not reciprocal to StoB
+func BtoS(b bool) string {
+	if b {
+		return "true"
+	}
+	return "false"
+}
--- gonverter_test.go
+++ gonverter_test.go
@@ -11,29 +11,60 @@ func TestItoS(t *testing.T) {
 
 // Test string to int conversion
 func TestStoIE(t *testing.T) {
+	msg := "StoI function didn't work as expected"
+	// OK
 	i, err := StoIE("42")
 	if i != 42 || err != nil {
-		t.Error("StoI function didn't work as expected")
-	}
-}
-
-// Test string to int conversion with errors
-func TestStoIE_Error(t *testing.T) {
-	i, err := StoIE("44abs")
-	msg := "StoI function didn't work as expected"
-
-	if err == nil {
 		t.Error(msg)
 	}
-	if i == 44 {
+	// Wrong
+	i, err = StoIE("44abs")
+	if err == nil || i == 44 {
 		t.Error(msg)
 	}
 }
 
 // Test string to int conversion
 func TestStoI(t *testing.T) {
-	i := StoI("42")
-	if i != 42 {
+	i1 := StoI("42")
+	i2, _ := StoIE("42")
+	if i1 != i2 {
 		t.Error("StoI function didn't work as expected")
+	}
+}
+
+// Test int to bool conversion
+func TestItoB(t *testing.T) {
+	b1 := ItoB(42)
+	b2 := ItoB(0)
+	if !b1 || b2 {
+		t.Error("ItoB function didn't work as expected")
+	}
+}
+
+//Test bool to int conversion
+func TestBtoI(t *testing.T) {
+	i1 := BtoI(true)
+	i2 := BtoI(false)
+	if i1 != 1 || i2 != 0 {
+		t.Error("BtoI function didn't work as expected")
+	}
+}
+
+// Test string to bool conversion
+func TestStoB(t *testing.T) {
+	s1 := StoB("Hello")
+	s2 := StoB("")
+	if !s1 || s2 {
+		t.Error("StoB function didn't work as expected")
+	}
+}
+
+//Test bool to string conversion
+func TestBtoS(t *testing.T) {
+	s1 := BtoS(true)
+	s2 := BtoS(false)
+	if s1 != "true" || s2 != "false" {
+		t.Error("BtoS function didn't work as expected")
 	}
 }
